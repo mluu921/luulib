3d9d4e50dbc70e040ab187eb8b529a54ecfc872e
Michael Luu <mluu921@gmail.com>
2020-11-10 15:37:02 -0800

    fix knit function to default to html

--- R/knit_with_date.R
+++ R/knit_with_date.R
@@ -5,7 +5,7 @@ knit_with_date <- function(input, ...) {
   rmarkdown::render(
     input,
     output_file = paste0(
-      xfun::sans_ext(input), '-', Sys.Date(), '.',
+      xfun::sans_ext(input), '-', Sys.Date(), '.html',
       xfun::file_ext(input)
     ),
     envir = globalenv()
